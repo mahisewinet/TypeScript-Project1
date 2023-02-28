cd4a41ffb095157669e0eba8374ce1d713ca0f6a
Mahi Sewinet <mahlet.sewinet@aau.edu.et>
2023-02-28 10:04:36 +0300

    Practice on class

---
+++ PracticeClass /Customer.js
@@ -0,0 +1,10 @@
+var Customer = /** @class */ (function () {
+    function Customer() {
+    }
+    return Customer;
+}());
+var myCustomer = new Customer();
+myCustomer.FName = "Mahlet";
+myCustomer.LName = "Sewinet";
+console.log(myCustomer.FName);
+console.log(myCustomer.LName);
---
+++ PracticeClass /Customer.ts
@@ -0,0 +1,13 @@
+class Customer{
+    FName: string;
+    LName: string;
+
+}
+
+let myCustomer= new Customer();
+
+myCustomer.FName="Mahlet";
+myCustomer.LName="Sewinet";
+
+console.log(myCustomer.FName);
+console.log(myCustomer.LName);
