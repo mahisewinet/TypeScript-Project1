785818babd5b8afa09dbd5afe4fc58462f29c62b
Mahi Sewinet <mahlet.sewinet@aau.edu.et>
2023-02-28 09:40:46 +0300

    simpler for loop

---
+++ sport.js
@@ -0,0 +1,5 @@
+var sport = ["Cricket", "Golf", "Swimming"];
+for (var _i = 0, sport_1 = sport; _i < sport_1.length; _i++) {
+    var tempSport = sport_1[_i];
+    console.log(tempSport);
+}
---
+++ sport.ts
@@ -0,0 +1,5 @@
+let sport: string[]= ["Cricket", "Golf","Swimming"];
+
+for(let tempSport of sport){
+    console.log(tempSport);
+}
