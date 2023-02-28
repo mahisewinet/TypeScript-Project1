67cd6a44af8f674acd21f68a86a1ee3804dfae5b
Mahi Sewinet <mahlet.sewinet@aau.edu.et>
2023-02-28 09:34:00 +0300

    add loop to first.js

--- first.js
+++ first.js
@@ -1,4 +1,10 @@
+"use strict";
+exports.__esModule = true;
 var found = true;
 var grade = 93.5;
+var review = [6.5, 7.5, 9.5];
 console.log(found);
 console.log("Hi my grade is ".concat(grade));
+for (var i = 0; i < review.length; i++) {
+    console.log(review[i]);
+}
