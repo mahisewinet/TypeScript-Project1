89a7a3d1616fe57481908ae1d0768b063f518cc1
Mahi Sewinet <mahlet.sewinet@aau.edu.et>
2023-02-28 09:31:48 +0300

    add for loop

--- first.ts
+++ first.ts
@@ -1,5 +1,13 @@
+export {};
+
 let found: boolean = true;
 let grade: number = 93.5;
 
+let review: number[]= [6.5,7.5,9.5];
+
 console.log(found);
 console.log(`Hi my grade is ${grade}`);
+
+for(let i=0; i<review.length; i++){
+    console.log(review[i]);
+}
