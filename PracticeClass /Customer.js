e72eb35a6d1d87a3a68ad24759ac13db7add35ee
Mahi Sewinet <mahlet.sewinet@aau.edu.et>
2023-02-28 10:07:37 +0300

    Constructor

--- PracticeClass /Customer.js
+++ PracticeClass /Customer.js
@@ -1,10 +1,10 @@
 var Customer = /** @class */ (function () {
-    function Customer() {
+    function Customer(theFName, theLName) {
+        this.FName = theFName;
+        this.LName = theLName;
     }
     return Customer;
 }());
-var myCustomer = new Customer();
-myCustomer.FName = "Mahlet";
-myCustomer.LName = "Sewinet";
+var myCustomer = new Customer("Almaz", "Dechasa");
 console.log(myCustomer.FName);
 console.log(myCustomer.LName);
--- PracticeClass /Customer.ts
+++ PracticeClass /Customer.ts
@@ -2,12 +2,13 @@ class Customer{
     FName: string;
     LName: string;
 
+    constructor(theFName:string,theLName:string){
+        this.FName= theFName;
+        this.LName= theLName;
+    }
 }
 
-let myCustomer= new Customer();
-
-myCustomer.FName="Mahlet";
-myCustomer.LName="Sewinet";
+let myCustomer= new Customer("Almaz","Dechasa");
 
 console.log(myCustomer.FName);
 console.log(myCustomer.LName);
