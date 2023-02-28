a95896924a4af9e341c48b3fe128f6a4ee1607ca
Mahi Sewinet <mahlet.sewinet@aau.edu.et>
2023-02-28 09:23:41 +0300

    first commit

---
+++ first.js
@@ -0,0 +1,4 @@
+var found = true;
+var grade = 93.5;
+console.log(found);
+console.log("Hi my grade is ".concat(grade));
---
+++ first.ts
@@ -0,0 +1,5 @@
+let found: boolean = true;
+let grade: number = 93.5;
+
+console.log(found);
+console.log(`Hi my grade is ${grade}`);
